68b23edcbfe6d836e592d9b73eb9069a36557351
Tiago Vieira <tiagoovieira@gmail.com>
2013-07-06 18:18:26 +0000

    try no3

--- web.js
+++ web.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var fs = require('fs');
-var buf = new Buffer('index.html');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
