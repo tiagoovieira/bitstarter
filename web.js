813d9236ace9af5d7afc305b823fa3b955e9012d
Tiago Vieira <tiagoovieira@gmail.com>
2013-07-06 18:26:54 +0000

    try no4

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
-
+var buf = new Buffer('index.html');
 app.get('/', function(request, response) {
 	fs.readFileSync('index.html', function(err, contents){
+		buf.toString('utf8',0, buf.length);
 		response.send(contents);
 	});
 });
